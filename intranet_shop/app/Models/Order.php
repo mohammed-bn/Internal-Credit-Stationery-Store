760e32a44c92d22a4391d43e233d2f439a2a9750
mohammed-bn <mbentiss11@gmail.com>
2026-02-05 14:34:38 +0100

    feat: add HasFactory trait and fillable properties to Order model

--- intranet_shop/app/Models/Order.php
+++ intranet_shop/app/Models/Order.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    use HasFactory;
+
+    protected $fillable = ['status','user_id','product_id'];
+
     public function Manager(){
         return $this->belongsTo(Manager::class);
     }
