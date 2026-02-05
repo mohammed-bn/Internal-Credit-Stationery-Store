e958aeba5fad97de21b962a976e13d7133bb2507
mohammed-bn <mbentiss11@gmail.com>
2026-02-05 14:34:43 +0100

    feat: add HasFactory trait and fillable properties to Product model

--- intranet_shop/app/Models/Product.php
+++ intranet_shop/app/Models/Product.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = ['name','price','is_premium'];
+
     public function Orders(){
         return $this->belongsTo(Order::class);
     }
