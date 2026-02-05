b23b072542aa8be8b65e008ff66dac7149c33ff1
mohammed-bn <mbentiss11@gmail.com>
2026-02-05 14:38:03 +0100

    feat: create migration files for cache and sessions tables, and add timestamps to managers and employees tables

--- intranet_shop/database/migrations/2026_02_02_142158_create_managers_table.php
+++ intranet_shop/database/migrations/2026_02_02_142158_create_managers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->integer('tokens');
             $table->integer('total');
             $table->foreignId('user_id')->constrained();
+            $table->timestamps();
         });
     }
 
