754e09644130163bf7a4de972f61fa94824d6fc2
mohammed-bn <mbentiss11@gmail.com>
2026-02-05 14:35:56 +0100

    feat: create RoleFactory for generating Role model instances

---
+++ intranet_shop/database/factories/RoleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+use App\Model\Product;
+use App\Model\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Role>
+ */
+class RoleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name'=>fake()->randomElement(['admin', 'manager']),
+        ];
+    }
+}
