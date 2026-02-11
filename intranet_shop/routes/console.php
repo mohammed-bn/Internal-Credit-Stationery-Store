8b4f6b8540b67736a7f8bfbc48c8b7a6804e2cff
mohammed-bn <mbentiss11@gmail.com>
2026-02-11 11:04:12 +0100

    feat: add scheduled task to reload tokens for employees and managers

--- intranet_shop/routes/console.php
+++ intranet_shop/routes/console.php
@@ -1,8 +1,22 @@
 <?php
-
+use App\Jobs\ProcessUserTokens;
+use App\Models\Employee;
+use App\Models\manager;
+use App\Models\User;
+use Illuminate\Support\Facades\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+   //reload tokens
+   $employes = DB::table('employees')->get();
+   $managers = DB::table('managers')->get();
+   DB::table('employees')
+        ->update(array('tokens' => 1000));  
+  DB::table('managers')
+        ->update(array('tokens' => 1000));
+
+})->everyFiveSeconds();
